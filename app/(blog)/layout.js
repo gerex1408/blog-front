ad6e4ecb492d8d085b3425c01267c6473ccea236
Gerard Lozano <gerard.lozano@fundcraft.lu>
2023-08-03 16:13:58 +0200

    removed useless root layouts

--- app/(blog)/layout.js
+++ app/(blog)/layout.js
@@ -1,23 +1,10 @@
-import '../global.css';
-
-import { Roboto_Flex } from 'next/font/google';
-import { Providers } from '../services/providers';
 import Navbar from '../components/Navbar';
 
-const roboto = Roboto_Flex({
-  subsets: ['latin'],
-  display: 'swap',
-});
-
-export default function RootLayout({ children }) {
+export default function BlogLayout({ children }) {
   return (
-    <html lang='en' className={roboto.className}>
-      <body>
-        <Providers>
-          <Navbar />
-          {children}
-        </Providers>
-      </body>
-    </html>
+    <>
+      <Navbar />
+      {children}
+    </>
   );
 }
